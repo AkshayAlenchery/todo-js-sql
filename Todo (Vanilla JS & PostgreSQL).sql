6853d9a3d2a8f6212ab5920d79aa0748ef7c9206
Akshay Alenchery <akshayalenchery@gmail.com>
2020-01-09 13:41:15 +0530

    Added database file

---
+++ Todo (Vanilla JS & PostgreSQL).sql
@@ -0,0 +1,24 @@
+/**
+* Name: List Table.
+* Desc: Used to store all the Todo lists.
+*/
+CREATE TABLE "lists" (
+  "list_id" SERIAL PRIMARY KEY NOT NULL,
+  "list_name" VARCHAR (20) NOT NULL
+);
+
+/**
+* Name: Todo table.
+* Desc: Used to store all the todos of the list.
+*/
+CREATE TABLE "todos" (
+  "todo_id" SERIAL PRIMARY KEY NOT NULL,
+  "list_id" INT NOT NULL,
+  "todo_name" VARCHAR (20) NOT NULL,
+  "completed" BOOLEAN NOT NULL DEFAULT false,
+  "priority" INT NOT NULL DEFAULT 0,
+  "scheduled" VARCHAR (10) DEFAULT null,
+  "note" TEXT DEFAULT null
+);
+
+ALTER TABLE "todos" ADD FOREIGN KEY ("list_id") REFERENCES "lists" ("list_id");
